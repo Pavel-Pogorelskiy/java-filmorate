33bf3a519c745bc0ad570935edefa0f29cd64708
Pavel Pogorelskiy <akantor23@yandex.ru>
2023-11-30 01:10:35 +0300

    Codestyle 4

--- src/main/java/ru/yandex/practicum/filmorate/storage/AbstractStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/AbstractStorage.java
@@ -6,9 +6,12 @@ import java.util.List;
 
 public interface AbstractStorage<T extends BaseUnit> {
     T create(T data);
+
     T uptade(T data);
     T get(int id);
+
     List<T> getAll();
+
     void delete(int id);
 
 }
