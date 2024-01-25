61e069f29737d3e6f61a7a96235e949abcfcfa20
Pavel Pogorelskiy <akantor23@yandex.ru>
2024-01-25 04:18:16 +0300

    Codestyle5

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -2,5 +2,5 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Genre;
 
-public interface GenreStorage extends AbstractStorage<Genre>{
+public interface GenreStorage extends AbstractStorage<Genre> {
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaStorage.java
@@ -2,5 +2,5 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Mpa;
 
-public interface MpaStorage extends AbstractStorage<Mpa>{
+public interface MpaStorage extends AbstractStorage<Mpa> {
 }
