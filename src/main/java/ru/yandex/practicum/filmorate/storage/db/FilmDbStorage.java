15ada12402cb89ab68fd2f98f5dfdb7d801f455d
Pavel Pogorelskiy <akantor23@yandex.ru>
2024-01-31 23:34:00 +0300

    Codestyle 1

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
@@ -179,7 +179,7 @@ public class FilmDbStorage implements FilmStorage {
         return genreMap;
     }
 
-    static public List<Film> fillGenres(List<Film> films, JdbcTemplate jdbcTemplate) {
+    static List<Film> fillGenres(List<Film> films, JdbcTemplate jdbcTemplate) {
         if (films.isEmpty()) {
             return films;
         }
