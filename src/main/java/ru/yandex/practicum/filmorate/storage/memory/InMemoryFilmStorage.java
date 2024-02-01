d6edd6baeb7da0c36c8dad41f4ad50faceddfd5b
akarui-shi <mmar1yakucherova@yandex.ru>
2024-02-01 19:30:13 +0300

    add directors

--- src/main/java/ru/yandex/practicum/filmorate/storage/memory/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/memory/InMemoryFilmStorage.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
+import java.util.List;
+
 @Component
 public class InMemoryFilmStorage extends InMemoryBaseStorage<Film> implements FilmStorage {
+    @Override
+    public List<Film> getFilmsByDirector(int directorId) {
+        return null;
+    }
 }
