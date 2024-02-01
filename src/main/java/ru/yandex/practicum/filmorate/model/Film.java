c86b82933af8c867d8af207ed2117997a6524142
akarui-shi <mmar1yakucherova@yandex.ru>
2024-02-01 20:05:52 +0300

    tests

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -30,5 +30,7 @@ public class Film extends BaseUnit {
     @NotNull
     @Builder.Default
     private List<Genre> genres = new ArrayList<>();
+    @NotNull
+    @Builder.Default
     private Set<Director> directors = new HashSet<>();
 }
